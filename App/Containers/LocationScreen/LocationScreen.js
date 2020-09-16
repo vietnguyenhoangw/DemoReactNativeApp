ff29f0ed7710e05f130f6300610879c5da2cd2b1
vietnguyenhoangw <hoang-viet.nguyen@ant-tech.eu>
2020-09-16 23:06:02 +0700

    Implement location :earth:

--- App/Containers/LocationScreen/LocationScreen.js
+++ App/Containers/LocationScreen/LocationScreen.js
@@ -3,14 +3,54 @@ import {Alert, AppStateStatus} from 'react-native';
 
 // function
 import {checkPermissionLocation} from '../../Functions/PermissionFunction';
+import {
+  clearWatchPosition,
+  getCurrentPosition,
+  stopObserving,
+  watchPosition,
+} from '../../Functions/GeolocationFunction';
 
 // redux
 import {useSelector, useDispatch} from 'react-redux';
 import AppActions from '../../Redux/AppRedux';
 
+let previousLocation = {
+  latitude: 0,
+  longitude: 0,
+};
 
 const LocationScreen = () => {
   const dispatch = useDispatch();
+  const isAllowLocation = useSelector((state) => state.app.isAllowLocation);
+
+  const handleLocation = (position, error) => {
+    if (error) {
+      console.log(error.message);
+      return;
+    }
+    if (position) {
+      const {latitude, longitude} = position.coords;
+      if (
+        !(
+          previousLocation.latitude === latitude ||
+          previousLocation.longitude === longitude
+        )
+      ) {
+        previousLocation.longitude = longitude;
+        console.log("handleLocation -> longitude", longitude)
+        console.log("handleLocation -> previousLocation.longitude", previousLocation.longitude)
+        previousLocation.latitude = latitude;
+        dispatch(AppActions.setLocation({latitude, longitude}));
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (isAllowLocation) {
+      getCurrentPosition(handleLocation);
+      watchPosition(handleLocation);
+    }
+  }, [isAllowLocation]);
 
   useEffect(() => {
     checkPermissionLocation((result) => {
--- App/Redux/AppRedux.js
+++ App/Redux/AppRedux.js
@@ -3,23 +3,30 @@ import Immutable from 'seamless-immutable';
 
 /* ------------- Types and Action Creators ------------- */
 const {Types, Creators} = createActions({
-    setAllowLocation: ['isAllowLocation'],
+  setAllowLocation: ['isAllowLocation'],
+
+  setLocation: ['location'],
 });
 
-export const AppTypes = Types
-export default Creators
+export const AppTypes = Types;
+export default Creators;
 
 /* ------------- Initial State ------------- */
 export const INITIAL_STATE = Immutable({
-    isAllowLocation: false,
-  });
+  isAllowLocation: false,
+
+  location: {}
+});
 
 /* ------------- Reducers ------------- */
-export const setAllowLocation = (
-    state, { isAllowLocation }
-  ) => state.merge({ isAllowLocation })
+export const setAllowLocation = (state, {isAllowLocation}) =>
+  state.merge({isAllowLocation});
+
+export const setLocation = (state, {location}) =>
+  state.merge({location});
 
 /* ------------- Hookup Reducers To Types ------------- */
 export const reducer = createReducer(INITIAL_STATE, {
-    [Types.SET_ALLOW_LOCATION]: setAllowLocation,
-  });
+  [Types.SET_ALLOW_LOCATION]: setAllowLocation,
+  [Types.SET_LOCATION]: setLocation,
+});
