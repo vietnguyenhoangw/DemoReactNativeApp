a5c31a289eba556678ff671b2e684c36d96604a2
vietnguyenhoangw <hoang-viet.nguyen@ant-tech.eu>
2020-10-06 15:40:19 +0700

    Implementing Push notification controller :construction:

---
+++ App/Containers/PushNotificationController.js
@@ -0,0 +1,37 @@
+import {useEffect, useRef} from 'react';
+import OneSignal from 'react-native-onesignal';
+
+const PushNotificationController = () => {
+  OneSignal.init('8849fb72-df4e-4865-9ec9-18ca791371ce', {
+    kOSSettingsKeyAutoPrompt: true,
+  });
+
+  OneSignal.enableVibrate(true);
+  OneSignal.enableSound(true);
+  OneSignal.inFocusDisplaying(2);
+
+  const onReceived = (notification) => {
+    console.log('onReceived -> notification', notification);
+  };
+  const onOpened = (openResult) => {
+    console.log('onOpened -> openResult', openResult);
+  };
+  const onIds = (device) => {
+    console.log('onIds -> device', device);
+  };
+
+  useEffect(() => {
+    OneSignal.addEventListener('received', onReceived);
+    OneSignal.addEventListener('opened', onOpened);
+    OneSignal.addEventListener('ids', onIds);
+    return () => {
+      OneSignal.removeEventListener('received', onReceived);
+      OneSignal.removeEventListener('opened', onOpened);
+      OneSignal.removeEventListener('ids', onIds);
+    };
+  }, []);
+
+  return null;
+};
+
+export default PushNotificationController;
