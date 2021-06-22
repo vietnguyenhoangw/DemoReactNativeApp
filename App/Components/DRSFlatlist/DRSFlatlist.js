23edfb153b6dc8fd8c716f943508d3cb39dfc233
vietnguyenhoangw <viet.nguyen@itbeesolutions.vn>
2021-06-22 15:07:47 +0700

    :hammer: optimize flatlist

--- App/Components/DRSFlatlist/DRSFlatlist.js
+++ App/Components/DRSFlatlist/DRSFlatlist.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, {useCallback} from 'react';
 import {View, FlatList} from 'react-native';
 
 // style
@@ -6,6 +6,8 @@ import styles from './Styles/DRSFlatlistStyle';
 
 // component
 import {DRSPostCard} from '../index';
+
+const ItemHeight = 460;
 
 function DRSFlatlist({listData, renderHeader, onRefresh}) {
   const processRenderHeader = () => {
@@ -15,22 +17,30 @@ function DRSFlatlist({listData, renderHeader, onRefresh}) {
     return null;
   };
 
-  const renderItem = ({item}) => {
-    return <DRSPostCard item={item} />;
-  };
+  const renderItem = useCallback(({item}) => <DRSPostCard item={item} />, []);
+
+  const keyExtractor = useCallback((item) => item._id.toString(), []);
+
+  const getItemLayout = (data, index) => ({
+    length: ItemHeight,
+    offset: ItemHeight * index,
+    index,
+  });
 
   return (
     <View style={[styles.container]}>
       {listData ? (
         <View>
           <FlatList
+            initialNumToRender={3}
             ListHeaderComponent={processRenderHeader}
             showsVerticalScrollIndicator={false}
             data={listData}
             renderItem={renderItem}
-            keyExtractor={(item) => item._id}
+            keyExtractor={keyExtractor}
             onRefresh={() => onRefresh()}
             refreshing={false}
+            getItemLayout={getItemLayout}
           />
         </View>
       ) : (
--- App/Components/DRSPostCard/Styles/DRSPostCardStyle.js
+++ App/Components/DRSPostCard/Styles/DRSPostCardStyle.js
@@ -1,70 +1,71 @@
-import React from 'react'
-import { StyleSheet } from 'react-native'
+import React from 'react';
+import {StyleSheet} from 'react-native';
 
 // theme
-import { Metrics, Colors } from '../../../Themes'
+import {Metrics, Colors} from '../../../Themes';
 
 export default StyleSheet.create({
-    container: {
-        flex: 1,
-        margin: Metrics.doubleBaseMargin,
-        marginBottom: Metrics.baseMargin,
-        backgroundColor: Colors.white, 
-        borderRadius: 20,
-        shadowColor: '#0000',
-        shadowOffset: {
-          width: 0,
-          height: 2
-        },
-        shadowOpacity: 0.23,
-        shadowRadius: 2.62,
-        elevation: 2
+  container: {
+    flex: 1,
+    margin: Metrics.doubleBaseMargin,
+    marginBottom: Metrics.baseMargin,
+    backgroundColor: Colors.white,
+    borderRadius: 20,
+    shadowColor: '#0000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    header: {
-        flexDirection: 'row',
-        marginTop: Metrics.baseMargin,
-        marginHorizontal: Metrics.baseMargin * 2,
-        alignItems: 'center',
-    },
-    avatar: {
-        height: 40,
-        width: 40,
-        borderRadius: 20,
-        marginTop: Metrics.baseMargin,
-    },
-    userNameText: {
-        fontWeight: 'bold'
-    },
-    dateText: {
-        fontSize: 11
-    },
-    nameContain: {
-        margin: Metrics.smallMargin - 2,
-        marginLeft: Metrics.smallMargin + 2,
-        flex: 1
-    },
-    bottom: {
-        marginTop: Metrics.baseMargin,
-        flexDirection: 'row',
-        flex: 1,
-        justifyContent: 'space-between',
-    },
-    btnBottomContainer: {
-        flex: 1,
-        alignItems: 'center',
-        padding: 15
-    },
-    post: {
-        marginTop: Metrics.baseMargin,
-        width: '100%',
-        height: Metrics.screenHeight / 3,
-        backgroundColor: Colors.black
-    },
-    btnBottom: {
-        tintColor: Colors.black
-    },
-    description: {
-        marginHorizontal: Metrics.baseMargin,
-        marginBottom: Metrics.baseMargin
-    }
-})
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 2,
+    height: 440,
+  },
+  header: {
+    flexDirection: 'row',
+    marginTop: Metrics.baseMargin,
+    marginHorizontal: Metrics.baseMargin * 2,
+    alignItems: 'center',
+  },
+  avatar: {
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+    marginTop: Metrics.baseMargin,
+  },
+  userNameText: {
+    fontWeight: 'bold',
+  },
+  dateText: {
+    fontSize: 11,
+  },
+  nameContain: {
+    margin: Metrics.smallMargin - 2,
+    marginLeft: Metrics.smallMargin + 2,
+    flex: 1,
+  },
+  bottom: {
+    marginTop: Metrics.baseMargin,
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  btnBottomContainer: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 15,
+  },
+  post: {
+    marginTop: Metrics.baseMargin,
+    width: '100%',
+    height: Metrics.screenHeight / 3,
+    backgroundColor: Colors.black,
+  },
+  btnBottom: {
+    tintColor: Colors.black,
+  },
+  description: {
+    marginHorizontal: Metrics.baseMargin,
+    marginBottom: Metrics.baseMargin,
+  },
+});
